ad478c98f133afab5bc9cd29409c67d4c2a1e642
Omar Aleiser Alvarez Laverde <omaraleyser@hotmail.com>
2022-10-23 22:15:12 -0500

    Barra desplamiento

--- Javascript/scritp.js
+++ Javascript/scritp.js
@@ -1,3 +1,4 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 // ======Funcion para el modal perfil======
 var dialog = document.getElementById("myDialog");
 document.getElementById("show").onclick = function () {
@@ -6,4 +7,22 @@ document.getElementById("show").onclick = function () {
 document.getElementById("hide").onclick = function () {
   dialog.close();
 };
-// pie de pagina
+// barra desplazamiento
+$(document).ready(function() {
+  $('.backtotop').css({'display': 'none'});
+    var offset = 200;
+    var duration = 600;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.backtotop').fadeIn(duration);
+        } else {
+            $('.backtotop').fadeOut(duration);
+        }
+    });
+ 
+    $('.backtotop').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
