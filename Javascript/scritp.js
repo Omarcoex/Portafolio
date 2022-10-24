bc30ffaf5fef0e3cef30c93db6cb9c5508600c67
Omar Aleiser Alvarez Laverde <omaraleyser@hotmail.com>
2022-10-23 21:50:36 -0500

    Final contacto

---
+++ Javascript/scritp.js
@@ -0,0 +1,9 @@
+// ======Funcion para el modal perfil======
+var dialog = document.getElementById("myDialog");
+document.getElementById("show").onclick = function () {
+  dialog.showModal();
+};
+document.getElementById("hide").onclick = function () {
+  dialog.close();
+};
+// pie de pagina
